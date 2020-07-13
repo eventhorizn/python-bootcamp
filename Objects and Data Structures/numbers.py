f244241969fcb50f5278dd712edbe11d310d989d
eventhorizn <41446698+eventhorizn@users.noreply.github.com>
2020-07-13 08:32:12 -0400

    numbers portion done not sure if I'll continue, or just reserve this for the assignments

---
+++ Objects and Data Structures/numbers.py
@@ -0,0 +1,34 @@
+# Addition
+2+1
+
+# Subtraction
+2-1
+
+# Multiplication
+2*2
+
+# Division
+3/2
+
+# Floor Division
+7//4
+
+# Modulus (Remainder)
+7%4
+
+# Powers
+2**3
+
+# Roots
+4**0.5
+
+# Order of Operations
+2 + 10 * 10 + 3
+
+# Parenethesis Specify Order
+(2 + 10) * (10 + 3)
+
+
+
+# Variable Assignment
+
